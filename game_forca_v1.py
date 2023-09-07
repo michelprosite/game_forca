21c291a57250eb5afaf113f892cbb58307e1d796
Michel <michel.prosite@gmail.com>
2023-09-07 18:29:08 -0300

    Versão 1 finalizada

--- game_forca_v1.py
+++ game_forca_v1.py
@@ -1,10 +1,6 @@
 import random
 from os import system, name
 
-letras_erradas = []
-chance = 0
-palavras = ['uva', 'banana', 'laranja']
-
 # Função para limpar tela
 def limpa_tela():
     # Para limpar a tela caso seja windows
@@ -15,9 +11,54 @@ def limpa_tela():
         _=system('clear')
 
 
-# Mensagem de boas vindas
-print("*" * 40)
-print("\n Seja bem vindo ao jogo da forca_V1 \n")
-print("*" * 40)
-
 def game():
+    limpa_tela()
+    
+    # iniciando a lista para listar as letras erradas
+    letras_erradas = []
+    # Iniciando a variavel de chances
+    chances = 6
+    # lIsta de palavras
+    palavras = ['uva', 'banana', 'laranja', 'morango']
+
+    # Sorteando a palavra
+    palavra = random.choice(palavras)
+    
+    # Monta a extrutura oculta da palavra a ser descoberta
+    letras_descobertas = ['_' for letra in palavra]
+
+    # Mensagem de boas vindas
+    print("*" * 40)
+    print("\nSeja bem vindo ao jogo da forca_V1 \n")
+    print(f"Advinhe a palavra abaixo, você terá {chances} chances antes de finalizar o jogo! \n")
+    print("*" * 40)
+
+    # Estrutura de loop do jogo
+    while chances > 0:
+        print(" ".join(letras_descobertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas:", " ".join(letras_erradas))
+
+        # tentativas
+        tentativa = input("\nDigite uma letra: ").lower()
+
+        if tentativa in palavra:
+            index = 0
+            for letra in palavra:
+                if tentativa == letra:
+                    letras_descobertas[index] = letra
+                index += 1
+        else:
+            chances -= 1
+            letras_erradas.append(tentativa)
+
+        if "_" not in letras_descobertas:
+            print("\nVocê venceu, a palavra era: ", palavra)
+            break
+
+    if "_" in letras_descobertas:
+        print("\nVocê perdeu, a palavra era: ", palavra)
+game()
+
+
+
